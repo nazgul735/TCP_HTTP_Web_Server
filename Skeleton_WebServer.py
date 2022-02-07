8bd1d021c334c6944673dcd27b213b8a5f7cdb9c
nazgul735 <68790240+nazgul735@users.noreply.github.com>
2022-02-07 18:34:53 +0100

    all set it´s running with localhost:6789/HelloWorld.html as local host

--- Skeleton_WebServer.py
+++ Skeleton_WebServer.py
@@ -1,36 +1,28 @@
-# This skeleton is valid for both Python 2.7 and Python 3.
-# You should be aware of your additional code for compatibility of the Python version of your choice.
-
-# Import socket module
+# Importing socket module
 from socket import *
 
-# Create a TCP server socket
+# Creating a TCP server socket
 # (AF_INET is used for IPv4 protocols)
 # (SOCK_STREAM is used for TCP)
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
+# Preparing a server socket.
+#host = '127.0.0.1'
+serverPort = 6789
 
-# Prepare a server socket.
-host = '127.0.0.1'
-serverPort = '6789'
-
-# Bind the socket to server address and server port
+# Binding the socket to server address and server port
 # A TCP based echo server
+serverSocket.bind(('', serverPort))
 
-echoSocket = socket.socket()
-echoSocket.bind((host, serverPort))
+# Listening to at most 1 connection at a time (max. number of threads in the connection queue)
 
-# Listen to at most 1 connection at a time (max. number of threads in the connection queue)
-
-echoSocket.listen(1)
-
-# FILL IN END
+serverSocket.listen(1)
 
 # Server should be up and running and listening to the incoming connections
 while True:
     print('Ready to serve...')
 
-    # Set up a new connection from the client
+    # new connection from the client
     connectionSocket, addr = serverSocket.accept()
 
     # If an exception occurs during the execution of try clause
@@ -41,29 +33,20 @@ while True:
         # Receives the request message from the client
         message = connectionSocket.recv(1024).decode()
 
-        # Extract the path of the requested object from the message
-        # The path is the second part of HTTP header, identified by [1]
+        # Extracting the path of the requested object from the message
         filepath = message.split()[1]
-        # filepath variable now contains the path to the requested object
-        # e.g. /HelloWorld.html
-        # Remember that the requested file must be in the same folder as the server code.
 
         # Because the extracted path of the HTTP request includes
         # a character '\', we read the path from the second character
         f = open(filepath[1:])
         # f variable now contains the file specified by the filepath
-
-        # Read the file "f" and store the entire content of the requested file in a temporary buffer
+        # Reading the file "f" and store the entire content of the requested file in a temporary buffer
         outputdata = f.read()
         # outputdata variable now contains the html code that the server is to send to the requesting client
-
-        # FILL IN START
 
         # Sending the HTTP response header line to the connection socket
         # The response should be in the following format: "HTTP/1.1 *code-for-successful-request*\r\n\r\n"
         connectionSocket.send(bytes('HTTP/1.1 200 OK\r\n\r\n', 'UTF-8'))
-
-        # FILL IN END
 
         # Send the content of the requested file to the connection socket
         response = outputdata + "\r\n"
@@ -74,16 +57,10 @@ while True:
         connectionSocket.close()
 
     except (IOError, IndexError):
-        # FILL IN START
         connectionSocket.send(bytes('HTTP/1.1 404 Not Found\r\n\r\n', 'UTF-8'))
-
-        # FILL IN END
         connectionSocket.send(
             "<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n")
 
-    # FILL IN START
-    # Close the client connection socket
-
-    # FILL IN END
+    connectionSocket.close()
 
 serverSocket.close()
